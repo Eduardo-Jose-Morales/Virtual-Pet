b11165a73609f69d9680437c1b99da5c8139e75f
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-20 15:25:12 +0000

    Creating all the signals that I'll need (pet, hunger, boredom, energy, age, isAlive message).

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { createSignal, createEffect } from 'solid-js'
 import './App.css'
 
 function App() {
+  const [pet, setPet] = createSignal('🥚');
+  const [hunger, setHunger] = createSignal(20);
+  const [boredom, setBoredom] = createSignal(20);
+  const [energy, setEnergy] = createSignal(100);
+  const [age, setAge] = createSignal(20);
+  const [isAlive, setIsAlive] = useState(true);
+  const [message, setMesssage] = createSignal('Take care of your pet!');
+
 
 
   return (
